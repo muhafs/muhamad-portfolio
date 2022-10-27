da8e00597147df964120cef278760f1555bcbcfd
muhamadhaspin <muhamadhaspin@gmail.com>
2022-10-27 12:11:40 +0700

    Changes to be committed: 	new file:   .gitignore 	new file:   .vscode/extensions.json 	new file:   README.md 	new file:   index.html 	new file:   package-lock.json 	new file:   package.json 	new file:   postcss.config.cjs 	new file:   public/vite.svg 	new file:   src/App.vue 	new file:   src/assets/images/anonymous.png 	new file:   src/assets/vue.svg 	new file:   src/components/NavBar.vue 	new file:   src/main.js 	new file:   src/pages/HomePage.vue 	new file:   src/router/index.js 	new file:   src/style.css 	new file:   tailwind.config.cjs 	new file:   vite.config.js

---
+++ src/components/NavBar.vue
@@ -0,0 +1,42 @@
+<template>
+	<header class="bg-transparent navbar-fixed top-0 left-0 w-full flex items-center z-50" :class="{ 'navba-fixed': burgerActive }">
+		<div class="container">
+			<div class="flex items-center justify-between relative">
+				<div class="px-4">
+					<router-link to="/" class="font-bold text-lg text-primary block py-6">muhamadhaspin</router-link>
+				</div>
+
+				<div class="flex items-center px-4">
+					<button @click="burgerActive = !burgerActive" type="button" class="block absolute right-4 lg:hidden" :class="{ 'hamburger-active': burgerActive }">
+						<span class="hamburger-line origin-top-left transition duration-300 ease-in-out"></span>
+						<span class="hamburger-line transition duration-300 ease-in-out"></span>
+						<span class="hamburger-line origin-bottom-left transition duration-300 ease-in-out"></span>
+					</button>
+
+					<nav v-show="burgerActive" class="absolute py-5 bg-white shadow-lg rounded-lg max-w-[250px] w-full right-4 top-full" :class="['lg:block']">
+						<ul class="block">
+							<li class="group">
+								<a href="#" class="flex text-base text-dark py-2 mx-8 group-hover:text-primary">Home</a>
+							</li>
+							<li class="group">
+								<a href="#" class="flex text-base text-dark py-2 mx-8 group-hover:text-primary">Home</a>
+							</li>
+							<li class="group">
+								<a href="#" class="flex text-base text-dark py-2 mx-8 group-hover:text-primary">Home</a>
+							</li>
+							<li class="group">
+								<a href="#" class="flex text-base text-dark py-2 mx-8 group-hover:text-primary">Home</a>
+							</li>
+						</ul>
+					</nav>
+				</div>
+			</div>
+		</div>
+	</header>
+</template>
+
+<script setup>
+	import { ref } from 'vue';
+
+	const burgerActive = ref(false);
+</script>
