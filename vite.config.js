3b6bce47ebe243544b9d36881ff88a9fc188f86c
muhamadhaspin <muhamadhaspin@gmail.com>
2022-11-01 07:57:58 +0700

    modified:   vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-        base: '/muhamad-portfolio/',
+	base: '/muhamad-portfolio/',
 	plugins: [vue()],
 	resolve: {
 		alias: {
